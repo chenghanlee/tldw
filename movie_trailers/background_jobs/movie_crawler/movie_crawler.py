3bb4d3367c7c00376e21bdd243a84b9e125f5cc2
Cheng-Han Lee <chenghan@udacity.com>
2013-10-13 11:40:37 -0700

    allow movies to be updated

--- movie_trailers/background_jobs/movie_crawler/movie_crawler.py
+++ movie_trailers/background_jobs/movie_crawler/movie_crawler.py
@@ -140,21 +140,27 @@ def save_movie_info_to_mongo(title, rt_id=None, save_similar_movies=False,
     thumbnail = create_thumbnail(formatted_title, poster, verbose=True)
     similar_movies_imdb_ids = convert_similar_movies_to_imdb_ids(
         similar_movies)
-
-    print "saving {title}".format(title=title)
-    new_movie = Movie(_director=director,
-                        _formatted_director=formatted_director,
-                        _title=title, _formatted_title=formatted_title, 
-                        _synopsis=synopsis, _critics_score=critics_score,
-                        _release_date=release_date, _poster=poster,
-                        _thumbnail=thumbnail, _cast=actors, _genres=genres,
-                        _metadata=metadata, _reviews=reviews,
-                        _similar_movies=similar_movies_imdb_ids,
-                        _trailers=trailers,
-                        _purchase_links=amazon_purchase_links)
-    new_movie.save()
-    index_movie(new_movie, verbose=True)
-    update_actors(actors, new_movie, verbose=True)
+    
+    args ={"_director"=director,
+            "_formatted_director"=formatted_director,
+            "_title"=title, "_formatted_title"=formatted_title, 
+            "_synopsis"=synopsis, "_critics_score"=critics_score,
+            "_release_date"=release_date, "_poster"=poster,
+            "_thumbnail"=thumbnail, "_cast"=actors, "_genres"=genres,
+            "_metadata"=metadata, "_reviews"=reviews,
+            "_similar_movies"=similar_movies_imdb_ids,
+            "_trailers"=trailers,
+            "_purchase_links"=amazon_purchase_links}
+    try:
+        print "saving {title}".format(title=title)
+        new_movie = Movie(args)
+        new_movie.save()
+        index_movie(new_movie, verbose=True)
+        update_actors(actors, new_movie, verbose=True)
+    except NotUniqueError:
+        print "updating {title}".format(title=title)
+        Movie.objects(_formatted_title=_formatted_title).update(
+            args)
 
     # save this movie's similar movies to mongo
     if save_similar_movies:
@@ -166,7 +172,7 @@ def save_movie_info_to_mongo(title, rt_id=None, save_similar_movies=False,
             print "queuing up {title}".format(title=title)
     
 if __name__ == "__main__":
-    title = "Harry Potter and the Goblet of Fire"
+    title = "Now You See Me"
     save_movie_info_to_mongo.delay(title, save_similar_movies=False, overwrite=True)
 
     
